390e59d351853206fcd6a5985e2d2ae19c4ee7d4
domgeorge <obinnadom007@gmail.com>
2024-01-08 17:38:03 -0800

    Replace element

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
+
+    myList = my_list[:]
+
+    if 0 <= idx < length:
+        newList[idx] = element
+
+    return (newList)
